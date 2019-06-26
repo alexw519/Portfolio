b131de7cbceac50939e60be23f7c8ccb435fdbf7
Alex Wilson <alexw519@gmail.com>
2019-06-25 19:11:34 -0500

    Formatting And Responsiveness

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,29 +1,32 @@
 $(document).ready(function()
 {
+    //Hide everything, and then show the About Me DIv
+    $("#about").hide();
     $("#contact").hide();
     $("#portfolio").hide();
+    $("#about").show(1000);
 
     //When clicked, only shows About Me
     $("#aboutClick").on("click", function()
     {
-        $("#about").show();
-        $("#contact").hide();
-        $("#portfolio").hide();
+        $("#about").show(2000);
+        $("#contact").hide(1000);
+        $("#portfolio").hide(1000);
     })
 
     //When clicked, only shows Contact
     $("#contactClick").on("click", function()
     {
-        $("#contact").show();
-        $("#about").hide();
-        $("#portfolio").hide();
+        $("#contact").show(2000);
+        $("#about").hide(1000);
+        $("#portfolio").hide(1000);
     })
 
     //When clicked, only shows Portfolio
     $("#portfolioClick").on("click", function()
     {
-        $("#portfolio").show();
-        $("#contact").hide();
-        $("#about").hide();
+        $("#portfolio").show(2000);
+        $("#contact").hide(1000);
+        $("#about").hide(1000);
     })
 })
